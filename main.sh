2b159fbbc3e43b6b3ee93823cc991e2bf1ffa744
Nesma-Mostafa <34743979+Nesma-Mostafa@users.noreply.github.com>
2018-01-08 07:45:59 -0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,27 @@
+#install tensorFlow
+$ sudo apt-get install python3-pip python3-dev python-virtualenv
+$ virtualenv --system-site-packages -p python3  targetDirectory
+$ source ~/tensorflow/bin/activate
+(tensorflow)$ easy_install -U pip
+(tensorflow)$ pip3 install --upgrade tensorflow
+$ source ~/tensorflow/bin/activate 
+$ python
+>>> import tensorflow as tf
+>>> quit()
+
+#install Keras
+$ mkvirtualenv keras_tf
+$ workon <keras_tf>
+$ pip install numpy scipy
+$ pip install scikit-learn
+$ pip install pillow
+$ pip install h5py
+$ pip install keras
+$ workon keras_tf
+$ python
+>>> import keras
+>>> quit()
+
+#install jellyFish
+sudo apt-get update
+sudo apt-get install jellyfish
