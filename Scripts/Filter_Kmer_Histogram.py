c1ae0fce3197c313ea148de7e0f0b4b143899519
Nesma-Mostafa <34743979+Nesma-Mostafa@users.noreply.github.com>
2018-01-08 03:17:22 +0200

    Update Filter_Kmer_Histogram.py

--- Scripts/Filter_Kmer_Histogram.py
+++ Scripts/Filter_Kmer_Histogram.py
@@ -1,9 +1,9 @@
-Python 3.5.2 (default, Nov 23 2017, 16:37:01) 
-[GCC 5.4.0 20160609] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> with open ("/home/nesma/Documents/Fastas/Histogram") as f:
+#Python 3.5.2 (default, Nov 23 2017, 16:37:01) 
+#[GCC 5.4.0 20160609] on linux
+#Type "copyright", "credits" or "license()" for more information.
+>>> with open ("your_Kmers_HistogramFile") as f:
 	data =f.readlines()
 	data =[item for item in data if int(item.split()[1]) > 1 ]
-	with open ("/home/nesma/Documents/Fastas/result.txt",'w') as rs:
+	with open ("yourOutputFile.txt",'w') as rs:     #flile to write the filtered kmers
 		for i in data:
 			rs.write(format(i))
