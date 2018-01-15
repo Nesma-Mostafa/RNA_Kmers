799cb3694dd54ae7492e27c41ed1eb5241c615af
Nesma-Mostafa <34743979+Nesma-Mostafa@users.noreply.github.com>
2018-01-15 20:26:45 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,24 +1,24 @@
 #install tensorFlow
-$ sudo apt-get install python3-pip python3-dev python-virtualenv
-$ virtualenv --system-site-packages -p python3  tensorflow
-$ source ~/tensorflow/bin/activate
-(tensorflow)$ easy_install -U pip
-(tensorflow)$ pip3 install --upgrade tensorflow
-$ source ~/tensorflow/bin/activate 
-$ python
+sudo apt-get install python3-pip python3-dev python-virtualenv
+virtualenv --system-site-packages -p python3  tensorflow
+source ~/tensorflow/bin/activate
+easy_install -U pip
+pip3 install --upgrade tensorflow
+source ~/tensorflow/bin/activate 
+ python
 >>> import tensorflow as tf
 >>> quit()
 
 #install Keras
-$ mkvirtualenv keras_tf
-$ workon <keras_tf>
-$ pip install numpy scipy
-$ pip install scikit-learn
-$ pip install pillow
-$ pip install h5py
-$ pip install keras
-$ workon keras_tf
-$ python
+ mkvirtualenv keras_tf
+ workon <keras_tf>
+ pip install numpy scipy
+ pip install scikit-learn
+ pip install pillow
+ pip install h5py
+ pip install keras
+ workon keras_tf
+ python
 >>> import keras
 >>> quit()
 
