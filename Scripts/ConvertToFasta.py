86eafd23815d9912ce4c48e9e8bdf8577ce173ec
Nesma-Mostafa <34743979+Nesma-Mostafa@users.noreply.github.com>
2018-03-12 01:49:50 +0200

    Update ConvertToFasta.py

--- Scripts/ConvertToFasta.py
+++ Scripts/ConvertToFasta.py
@@ -1,16 +1,8 @@
-import zipfile
 import os
 
-str=[]
-data_txt = []
-c=0
-for filename in os.listdir(os.getcwd()):
-	if(len(filename.split('.'))==3 and filename.split('.')[2]=="gz"):
-		str.append(filename)
-                data_txt.append(filename.split('.')[0]+".txt")
 
-c=0
 for fn in os.listdir(os.getcwd()):
+	c=0
         if(len(filename.split('.'))==2 and filename.split('.')[2]=="txt"):
 	with open(fn, 'r+') as f:
 		 content = f.read().splitlines()
